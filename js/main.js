e76fcdf37bf6886de22be689ff69b5b2d69f79fc
fana-github <fana-github@users.noreply.github.com>
2015-08-05 14:07:26 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/* NAV Menu*/
+$( "#trigger" ).click(function() {
+  $("#submenu-services").hide();
+  $("#mainmenu").toggle(300);
+});
+
+$("#mainmenu-services").click(function() {
+  $("#submenu-services").toggle(300);
+});
+
+$(window).resize(function() {
+  if(window.matchMedia('(max-width: 960px)').matches) {
+    $("#mainmenu").hide();
+  } else {
+    $("#mainmenu").show();
+  }
+});
+
+/*Anchor Tags Sprungfunktion*/
+$(document).ready(function() {
+$("a[href^=#]").click(function(e){
+        e.preventDefault();
+    
+    var href = $(this).attr('href').substring(1);
+    $('html, body').animate({
+        scrollTop:$("a[name="+href+"]").offset().top
+    },'slow');
+
+  
+    //$('html,body').animate({ scrollTop: 500 }, 300);
+});
+});
