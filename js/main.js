0d791ece44783b58a66b9ec2e2b228e8269514ba
fana-github <fana-github@users.noreply.github.com>
2015-08-05 16:17:18 +0200

    Update main.js
    document ready um komplettes js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
-/* NAV Menu*/
-$( "#trigger" ).click(function() {
-  $("#submenu-services").hide();
-  $("#mainmenu").toggle(300);
-});
-
-$("#mainmenu-services").click(function() {
-  $("#submenu-services").toggle(300);
-});
-
-$(window).resize(function() {
-  if(window.matchMedia('(max-width: 960px)').matches) {
-    $("#mainmenu").hide();
-  } else {
-    $("#mainmenu").show();
-  }
-});
-
-/*Anchor Tags Sprungfunktion*/
 $(document).ready(function() {
-  alert("test");
-$("a[href^=#]").click(function(e){
-        e.preventDefault();
-    
-    var href = $(this).attr('href').substring(1);
-    $('html, body').animate({
-        scrollTop:$("a[name="+href+"]").offset().top
-    },'slow');
 
+  /* NAV Menu*/
+  $( "#trigger" ).click(function() {
+    $("#submenu-services").hide();
+    $("#mainmenu").toggle(300);
+  });
+
+  $("#mainmenu-services").click(function() {
+    $("#submenu-services").toggle(300);
+  });
+
+  $(window).resize(function() {
+    if(window.matchMedia('(max-width: 960px)').matches) {
+      $("#mainmenu").hide();
+    } else {
+      $("#mainmenu").show();
+    }
+  });
+
+  /*Anchor Tags Sprungfunktion*/
+  alert("test");
+  $("a[href^=#]").click(function(e){
+      e.preventDefault();
+      var href = $(this).attr('href').substring(1);
+      $('html, body').animate({
+          scrollTop:$("a[name="+href+"]").offset().top
+      },'slow');
+    
+      //$('html,body').animate({ scrollTop: 500 }, 300);
+  });
   
-    //$('html,body').animate({ scrollTop: 500 }, 300);
-});
 });
