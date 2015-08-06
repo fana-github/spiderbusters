bd5a724f1e9ddc760c44778f5e29efc26bea8df2
fana-github <fana-github@users.noreply.github.com>
2015-08-06 14:35:30 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,9 +30,11 @@ $(document).ready(function() {
       if($('#trigger:visible').length != 0)
       {
         //mobile Ansicht => Komplettes Menü ausblenden
+        alert("1");
         $("#mainmenu").hide();
       } else {
         //Normale Ansicht 0> nur Submenü ausblenden
+        alert("2");
         $("#submenu-services").hide();
       }
       
