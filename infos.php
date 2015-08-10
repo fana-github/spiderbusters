1c3cb3865e2225994d91e34eafad00477221a49b
fana-github <fana-github@users.noreply.github.com>
2015-08-10 17:40:53 +0200

    Create infos.php

---
+++ infos.php
@@ -0,0 +1,51 @@
+<DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="style/main.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+	</head>
+	<body>
+        <div class="row">
+      		<div class="large-6 medium-6 small-8 phone-12">
+      		  <img id="logo" src="http://konstruktionsbuero-werthner.de/spiderbusters/logo.png" />
+      		</div>
+    	</div>
+    		
+      	<div id="impressum" class="row">
+      	    <div class="large-12 medium-12 small-12 phone-12">
+      	        <h2>Impressum</h2>
+      	    </div>
+      	    <div class="large-12 medium-12 small-12 phone-12">
+      	        tbd
+      	    </div>
+      	</div>
+      	
+      	<div id="agb" class="row">
+      	    <div class="large-12 medium-12 small-12 phone-12">
+      	        <h2>Geschäftsbedingungen</h2>
+      	    </div>
+      	    <div class="large-12 medium-12 small-12 phone-12">
+      	        tbd
+      	    </div>
+      	</div>
+	</body>
+	<footer>
+		<div class="row">
+			<div class="large-6">
+				<h3>Navigation</h3>
+				<a href="index.php#home">Home</a><br>
+				<a href="index.php#services">Leistungen</a><br>
+				<a href="index.php#support">Support</a><br>
+				<a href="index.php#contact">Kontakt</a>
+			</div>
+			<div class="large-6">
+				<h3>Links</h3>
+				<a href="#impressum">Impressum</a><br>
+				<a href="#agb">Geschäftsbedingungen</a>
+			</div>
+		</div>
+	</footer>
+</html>
