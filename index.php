77a95e21aad787bc9e93f3f31d56307c336ca684
fana-github <fana-github@users.noreply.github.com>
2015-08-10 21:50:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
 
 		<div class="row">
 			<div class="large-12 hero">
-		  		<img src="https://download.fotolia.com/Content/CompImage500/FotoliaComp_74421296_1ZYIDO8vWbJNMbWRTnbgRHUEKvhVdyNV_W95">
+		  		<img src="https://download.fotolia.com/Content/CompImage500/FotoliaComp_74440313_5jZl4bmDRpYqF15XtnRPMz6Xwp0CsWfn_W95">
 			</div>	
 		</div>
 		
