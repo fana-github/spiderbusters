2b5f4c97ba37733e791300cc4b2d0c598375ac0a
fana-github <fana-github@users.noreply.github.com>
2015-08-10 17:16:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -280,71 +280,7 @@
 				Wenn Sie Ihre benötigte Reparatur nicht finden oder individuelle Lösungen, wie ein z.B. ausgefallendes Backcover benötigen, kümmern wir uns gerne um Ihr Anliegen.
 				<br>Teilen Sie uns Ihr Anliegen mit und wir nehmen unverzüglich Kontakt zu Ihnen auf.
 				
-				<?php
-				
-				define('URL', 'http://www.alexander-psiuk.de/fana/');
- 
-				// ======= Konfiguration:
-				 
-				$mailTo = 'predator0815@gmx.de';
-				$mailFrom = '"Spiderbusters" <info@spiderbusters.de>';
-				$mailSubject    = 'Support Request SPIDERBUSTERS';
-				$mailText = "";
-				 
-				// ======= Text der Mail aus den Formularfeldern erstellen:
-				 
-				// Wenn Daten mit method="post" versendet wurden:
-				if(isset($_POST)) { 
-				   // alle Formularfelder der Reihe nach durchgehen:
-				   foreach($_POST as $name => $value) {
-				      // Wenn der Feldwert aus mehreren Werten besteht:
-				      // (z.B. <select multiple>)
-				      if(is_array($value)) {
-				          // "Feldname:" und Zeilenumbruch dem Mailtext hinzufügen
-				          $mailText .= $name . ":\n";
-				          // alle Werte des Feldes abarbeiten
-				          foreach($valueArray as $entry) {
-				             // Einrückungsleerzeichen, Wert und Zeilenumbruch 
-				             // dem Mailtext hinzufügen
-				             $mailText .= "   " . $value . "\n";
-				          } // ENDE: foreach
-				      } // ENDE: if  
-				      // Wenn der Feldwert ein einzelner Feldwert ist:
-				      else {
-				          // "Feldname:", Wert und Zeilenumbruch dem Mailtext hinzufügen
-				          $mailText .= $name . ": " . $value . "\n";
-				      } // ENDE: else
-				   } // ENDE: foreach
-				} // if
-				 
-				// ======= Korrekturen vor dem Mailversand 
-				 
-				// Wenn PHP "Magic Quotes" vor Apostrophzeichen einfügt:
-				 if(get_magic_quotes_gpc()) {
-				     // eventuell eingefügte Backslashes entfernen
-				     $mailtext = stripslashes($mailtext);
-				 }
-				 
-				// ======= Mailversand
-				 
-				// Mail versenden und Versanderfolg merken
-				$mailSent = @mail($mailTo, $mailSubject, $mailText, "From: ".$mailFrom);
-				 
-				// ======= Return-Seite an den Browser senden
-				 
-				// Wenn der Mailversand erfolgreich war:
-				if($mailSent == TRUE) {
-				   // Seite "Formular verarbeitet" senden:
-				   header('Location: '.URL.'index.php?supportmailsent=true');
-				}
-				// Wenn die Mail nicht versendet werden konnte:
-				else {
-				   // Seite "Fehler aufgetreten" senden:
-				   header('Location: '.URL.'index.php?supportmailsent=false');
-				}
-				?>
-				
-				<form method="post">
+				<form method="post" action="sendsupportmail.php">
 					<div>
 					  <label class="support_label large-4 medium-4 small-12 phone-12">Name:</label>
 					  <input class="large-8 medium-8 small-12 phone-12" type="text" id="support_fullname" name="full_name" placeholder="Max Mustermann" required />
